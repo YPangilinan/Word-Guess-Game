36067745edf10b5e395b8a21c75fc566a6851be2
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-04-21 11:45:44 -0700

    fixing js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,25 @@
 
 //computer choices for words
 var destinations = ["naples","budapest","bali","waco","singapore","tokyo","chicago", "london","prague","positano","dubai", "maldives"]
+//choose word randomly
+
+var computerGuess = destinations[Math.floor(Math.random() * destinations.length)];
+console.log(computerGuess);
+//create underscores based on length of word
+//user guess
+//check if value is true
+//is true, push to right array
+//if false, push to wrong array
+
 
 //max number of guesses
 const maxGuess=10
 
-//variables for all other elements of the game
-var guessedLetters = []
-var guessedWord = []
-var matchedWord
-var numGuess
-var wins = 0
+
 
 //at max guesses, restarts for a new word
-resetGame()
+//resetGame()
 
-//starter code from rock, paper, scissors game
-
-    // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
-
-        // Determines which key was pressed.
-        var guessedLetters = event.key;
-  
-        // Randomly chooses a choice from the options array. This is the Computer's guess.
-        var computerChoices = destinations[Math.floor(Math.random() * destinations.length)]}
-
-        //after user presses a key, need a for loop to search for letter
-        for (var i=0, j=matchedWord.length; i<j; i++){
-            if (letter === matchedWord[i]){
-                guessedWord[i] = letter
-            }
-        }
 
 
 
