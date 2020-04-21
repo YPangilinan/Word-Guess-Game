49e6fb39ec288a80c3cbfe2386d794370bff25c7
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-04-21 11:55:14 -0700

    further changes to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,19 @@
 
 //computer choices for words
 var destinations = ["naples","budapest","bali","waco","singapore","tokyo","chicago", "london","prague","positano","dubai", "maldives"]
-//choose word randomly
 
+//choose word randomly
 var computerGuess = destinations[Math.floor(Math.random() * destinations.length)];
-console.log(computerGuess);
+var underscore = []
+
 //create underscores based on length of word
+var generateUnderscore = function(){
+    for (var i=0; i<computerGuess.length; i++){
+        underscore.push('_'); 
+    }
+    return underscore;
+}
+
 //user guess
 //check if value is true
 //is true, push to right array
