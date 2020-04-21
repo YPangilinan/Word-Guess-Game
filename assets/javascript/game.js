7bf1140b6eb362076261eefe473dc2b0b8b0fa32
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-04-21 14:06:12 -0700

    beginning javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,47 +4,54 @@ var destinations = ["naples","budapest","bali","waco","singapore","tokyo","chica
 
 //choose word randomly
 var computerGuess = destinations[Math.floor(Math.random() * destinations.length)];
-var underscore = [];
 var rightLetter =[];
 var wrongLetter =[];
+var underScore =[];
 
-
+//testing
 console.log(computerGuess);
 
 //create underscores based on length of word
 var generateUnderscore = function(){
     for (var i=0; i<computerGuess.length; i++){
-        underscore.push('_'); 
-    }
-    return underscore;
+        underScore.push('_'); 
+    }return underScore;
 }
+
 console.log(generateUnderscore());
+
 //user guess
-document.onkeyup = function(event) {
-    var userGuess = event.key;
-    console.log(computerGuess.indexOf(userGuess));
+    document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 //if userGuess letter that is correct
     if ((computerGuess.indexOf(userGuess) > -1)){
+
 //add to the right letter array
         rightLetter.push(userGuess);
-        console.log(rightLetter);
+
+ //replacing underscore with letter       
+        underScore[computerGuess.indexOf(userGuess)] = userGuess;
+
+  //if all underscores filled, user wins and score on screen will increase   
+
+    if(underScore.join('') == computerGuess){
+        alert('you win!');
+    } 
+        
     }
+    //all wrong letters get pushed to other array and displayed on the screen
     else {
         wrongLetter.push(userGuess);
-        console.log(wrongLetter);
         }
+//once user gets right answer or reaches max guesses, the game will randomly choose another word
+
+
+console.log(computerGuess.indexOf(userGuess));
+console.log(rightLetter);
+console.log(wrongLetter);
+
 };
-//check if value is true
-
-
-//is true, push to right array
-//if false, push to wrong array
-
-
-//max number of guesses
-const maxGuess=10
-
 
 
 //at max guesses, restarts for a new word
