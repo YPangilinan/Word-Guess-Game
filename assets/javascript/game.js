11ed2d74d78e485caea86f1ae6005d161d866cfa
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-04-21 12:17:24 -0700

    fixing underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,12 @@ var destinations = ["naples","budapest","bali","waco","singapore","tokyo","chica
 
 //choose word randomly
 var computerGuess = destinations[Math.floor(Math.random() * destinations.length)];
-var underscore = []
+var underscore = [];
+var rightLetter =[];
+var wrongLetter =[];
+
+
+console.log(computerGuess);
 
 //create underscores based on length of word
 var generateUnderscore = function(){
@@ -13,9 +18,26 @@ var generateUnderscore = function(){
     }
     return underscore;
 }
-
+console.log(generateUnderscore());
 //user guess
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    console.log(computerGuess.indexOf(userGuess));
+
+//if userGuess letter that is correct
+    if ((computerGuess.indexOf(userGuess) > -1)){
+//add to the right letter array
+        rightLetter.push(userGuess);
+        console.log(rightLetter);
+    }
+    else {
+        wrongLetter.push(userGuess);
+        console.log(wrongLetter);
+        }
+};
 //check if value is true
+
+
 //is true, push to right array
 //if false, push to wrong array
 
